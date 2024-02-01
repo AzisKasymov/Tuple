b5ffb76f0607673194590a0fb1f0d0f444b8f828
AzisKasymov <abduazis.kasymov@alatoo.edu.kg>
2024-02-01 10:17:00 +0600

    Q2 laptop.py

---
+++ twain.py
@@ -0,0 +1,12 @@
+laptop = { "brand": "dell", "model": "alienware", "year": 2010 }
+
+#first
+print(laptop["brand"])
+
+#second
+laptop["home"] = True
+print(laptop)
+
+#third
+laptop["year"] = 2019
+print(laptop)
